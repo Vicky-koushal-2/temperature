3bc65a7f5f9cad3c67d496b7cac6e2038e363b69
Vicky kaushal <121958580+Vicky-koushal-2@users.noreply.github.com>
2023-01-17 23:21:09 +0530

    Add files via upload

---
+++ tem.js
@@ -0,0 +1,10 @@
+let celsius=document.getElementById('celsius');
+let fahrenhit =document.getElementById("fahrenhit");
+celsius.oninput=()=>{
+    let output = (parseFloat(celsius.value)*9)/5+32;
+    fahrenhit.value = parseFloat(output.toFixed(2));
+};
+celsius.oninput=()=>{
+    let output = ((parseFloat(fahrenhit.value)-32)/5)/9;
+    celsius.value = parseFloat(output.toFixed(2));
+};
